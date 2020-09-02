afa9bb95af292cc02220817d5a23f9fcb1afb95a
Chase Wohrle <chase.wohrle@gmail.com>
2020-09-02 16:34:53 -0700

    9/2 push

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(function() {
-  $("button").click(function(event) {
+  $(".js-additem").click(function(event) {
     $("ul").append(
       "<li>" + $("input").val() + "</li>"
     );
   });  
-  $("li").click(function(event) {
-    this.toggleClass();
+  $("shopping-item").click(function(event) {
+   $(this).find(".shopping-item").toggleClass("shopping-item__checked");
   });
-  $("").click(function(event) {
-    this.remove();
-  });
+  $("shopping-item").click(function(event) {
+   this.remove();
+ });
 });  
